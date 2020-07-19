33f7ebc4ef121540c28e458ef7ca47c32543ec64
saithat <sai.thatigotla@gmail.com>
2020-07-19 12:11:35 -0400

    wokring on url submissions

--- src/components/StreamGallery.js
+++ src/components/StreamGallery.js
@@ -11,6 +11,8 @@ import {
   useParams
 } from "react-router-dom";
 import Player from "./embedVideo";
+import urlForm from './urlForm';
+
 
 const vids = [
   {id: 0, link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"},
@@ -18,10 +20,37 @@ const vids = [
   {id: 2, link: "https://www.youtube.com/watch?v=_V2sBURgUBI&list=PLV2ewAgCPCq0DVamOw2sQSAVdFVjA6x78&index=43"}
 ]
 
+/*
+var vids = [
+  {link: "test"}
+];
+*/
+
+//<Player url={vids[this.props.match.params.ID].link} />
+
 class StreamGallery extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: "",
+      links: ""
+    };
+  }
+  onInputChange = value => {
+    this.setState({
+      links: value
+    });
+    vids.push({link: value})
+    console.log(vids);
+  }
   render() {
     return (
-      <Player url={vids[this.props.match.params.ID].link} />
+      <div>
+      <urlForm onChange={this.onInputChange} />
+        {vids.map(i => (
+          <Player url{...i.link} />
+        ))}
+      </div>
     );
   }
 }
