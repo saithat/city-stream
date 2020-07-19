ba52b2fc49d1a3703d48126313315dca1b51f413
saithat <sai.thatigotla@gmail.com>
2020-07-19 12:22:58 -0400

    id matching fix

--- src/components/StreamGallery.js
+++ src/components/StreamGallery.js
@@ -16,16 +16,23 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
 const vids = [
-  {id: 0, link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"},
-  {id: 1, link: "https://www.youtube.com/watch?v=ZZ5LpwO-An4&list=PLV2ewAgCPCq0DVamOw2sQSAVdFVjA6x78"},
-  {id: 2, link: "https://www.youtube.com/watch?v=_V2sBURgUBI&list=PLV2ewAgCPCq0DVamOw2sQSAVdFVjA6x78&index=43"}
+  {id: 2712, link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ"},
+  {id: 2713, link: "https://www.youtube.com/watch?v=ZZ5LpwO-An4&list=PLV2ewAgCPCq0DVamOw2sQSAVdFVjA6x78"},
+  {id: 2714, link: "https://www.youtube.com/watch?v=_V2sBURgUBI&list=PLV2ewAgCPCq0DVamOw2sQSAVdFVjA6x78&index=43"}
 ]
 
 class StreamGallery extends React.Component {
   render() {
+    var url;
+    for (var i in vids){
+      if(vids[i].id == this.props.match.params.ID)
+      {
+        url = vids[i].link;
+      }
+    }
     return (
       <div class="stream-gallery">
-        <Player url={vids[this.props.match.params.ID].link} />
+        <Player url={url} />
         <br></br>
         <Card style={{ padding: '20px', width: '50em' }}>
           <Card.Title>Add a Livestream</Card.Title>
